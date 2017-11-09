4089b2abd7e80a2c3510dc7c9306b2d28607ae82
leorosie <31524043+leorosie@users.noreply.github.com>
2017-11-08 17:16:22 -0700

    Update utils.h

--- src/utils.h
+++ src/utils.h
@@ -13,12 +13,11 @@
 inline uint8_t float_to_bytes(uint8_t* address, float float_val){
   union {
     float float_bytes;
-    uint8_t temp_array[4];
+    uint8_t float_array[4];
   } u;
   u.float_bytes = float_val;
-  memcpy(address, u.temp_array, 4);
-  //return(sizeof(u.temp_array));
-  return(u.float_bytes);
+  memcpy(address, u.float_array, 4);
+  return(sizeof(u.float_array));
 }
 
 /**
@@ -32,33 +31,48 @@ inline uint8_t float_to_bytes(uint8_t* address, float float_val){
 inline float bytes_to_float(uint8_t* address){
   union {
     float float_bytes;
-    uint8_t temp_array[4];
+    uint8_t float_array[4];
   } u;
   float f;
-  memcpy(u.temp_array, address, 4);
+  memcpy(u.float_array, address, 4);
   f = u.float_bytes;
   return(f);
 }
 
+/**
+*  \brief Convert a long to an array of bytes.
+*
+*  \param address the memory location where the long will be written as bytes
+*  \param long_val the number which will be written to bytes
+*  \return i the length of the byte string (should always be 8)
+*
+*/
+
 inline uint8_t long_to_bytes(uint8_t* address, long long_val){
   union {
     long long_bytes;
-    uint8_t sonic_array[8];
+    uint8_t long_array[8];
   } u;
   u.long_bytes = long_val;
-  memcpy(address, u.sonic_array, 8);
-  return(sizeof(u.sonic_array));
-  //LOOK INTO:
-  //return(u.long_bytes); QC and returns weird number for sonic ranger
+  memcpy(address, u.long_array, 8);
+  return(sizeof(u.long_array));
 }
+
+/**
+*  \brief Convert an array of bytes to a long.
+*
+*  \param address the memory location of the long (_always_ 8)
+*  \return l the long
+*
+*/
 
 inline long bytes_to_long(uint8_t* address){
   union {
     long long_bytes;
-    uint8_t sonic_array[8];
+    uint8_t long_array[8];
   } u;
   long l;
-  memcpy(u.sonic_array, address, 8);
+  memcpy(u.long_array, address, 8);
   l = u.long_bytes;
   return(l);
 }
