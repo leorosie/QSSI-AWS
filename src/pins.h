5f5e73cb925ecf33d12f5dbec090d5ff8c2c11cc
dave <david.blasen@umontana.edu>
2017-11-18 19:27:12 -0700

    created the SD card class and filled it out. haven't even tried to compile it. i'll try tomorrow.

--- src/pins.h
+++ src/pins.h
@@ -25,3 +25,7 @@
 const gpio_num_t WAKE_PIN = GPIO_NUM_39;
 const int SONIC_RANGER_BUS = 12;
 const int ONE_WIRE_BUS = 14;
+
+const int SCK_PIN = 18;
+const int MISO_PIN = 19;
+const int MOSI_PIN = 23;
